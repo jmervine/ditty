9ea083ab39b7ca2030ba5e5f0a81750e35cc9921
Joshua Mervine <joshua@mervine.net>
2012-05-08 16:41:08 -0700

    Added error handling. Better page titling. Etc.

--- ditty.rb
+++ ditty.rb
@@ -9,26 +9,67 @@ require 'pp'
 
 class Ditty < Sinatra::Application
 
+  enable :logging, :dump_errors, :raise_errors
+
   helpers do
     include TemplateHelpers
   end
 
   set :default_title, begin settings.config["default_title"] rescue "My little Ditty's!" end
 
-  get "/"         do; erb :index; end
-  get "/post"     do; erb :form_post, :locals => { :page_title => "Sing a little Ditty!" }; end
-  get "/edit/*"     do; erb :form_post, :locals => { :page_title => "Tweak your Ditty!", :path => File.join(settings.store, params[:splat].join) }; end
-  get "/archive/?*" do
-    y, m = params[:splat].join.split("/")
-    title = "Archive :: "
-    if m.nil?
-      title << y
-    else
-      title << "#{months[m.to_i-1].capitalize}, #{y}"
+  get "/new" do
+    begin
+      erb :form_post, :locals => { :page_title => "Sing a little Ditty!",
+                                    :navigation => :nav_help }
+    rescue
+      logger.info "error on : #{params.to_s}"
+      pass
     end
-    erb :archive, :locals => { :archive => find_f(File.join(settings.store, params[:splat])),
-                               :archive_title => title }
   end
+
+  get "/post/*" do
+    begin
+      path = File.join(settings.store, params[:splat].join)
+      erb :post, :locals => { :page_title => post_title(path), :path => path }
+    rescue Exception => e
+      logger.error e
+      pass
+    end
+  end
+
+  get "/edit/*" do
+    begin
+      erb :form_post, :locals => { :page_title => "Tweak your Ditty!", 
+                                    :path => File.join(settings.store, params[:splat].join),
+                                    :navigation => :nav_help }
+    rescue Exception => e
+      logger.error e
+      pass
+    end
+  end
+
+  get "/archive/?*" do
+    begin
+      y, m = params[:splat].join.split("/")
+      title = "Archive :: "
+      if m.nil?
+        title << y
+      else
+        title << "#{months[m.to_i-1].capitalize}, #{y}"
+      end
+      erb :archive, :locals => { :archive => find_f(File.join(settings.store, params[:splat])),
+                                 :archive_title => title }
+    rescue Exception => e
+      logger.error e
+      pass
+    end
+  end
+
+  # catch all others
+  get "/?*" do
+    erb :index; 
+  end
+
 
   #post "/post"    { @ditty.save params }
   #post "/edit"    { @ditty.update params }  
