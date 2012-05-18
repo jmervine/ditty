4f71f4a186fb1df9661fffc4e034cbdd8e79ba63
Joshua Mervine <joshua@mervine.net>
2012-05-18 09:29:14 -0700

    made post title a link to the post

--- spec/dittyapp_spec.rb
+++ spec/dittyapp_spec.rb
@@ -15,6 +15,9 @@ describe DittyApp, "< Sinatra::Application" do
     it "should have title" do
       last_response.should match Regexp.new(Regexp.escape("<title>My little Ditty's!</title>"))
     end
+    it "post titles should be a link" do
+      last_response.should match Regexp.new("<a href=\'\/post\/([a-z0-9]+)'>post title - (.+)</a>")
+    end
     it "should have new post link" do
       last_response.should match Regexp.new(Regexp.escape("new post</a>"))
     end
@@ -67,6 +70,9 @@ describe DittyApp, "< Sinatra::Application" do
     it "should have title" do
       last_response.should match Regexp.new(Regexp.escape("<title>My little Ditty's!</title>"))
     end
+    it "post title should be a link" do
+      last_response.should match Regexp.new("<a href=\'\/post\/([a-z0-9]+)'>post title - (.+)</a>")
+    end
     it "should not have new post link" do
       last_response.should_not match Regexp.new(Regexp.escape("new post</a>"))
     end
@@ -88,6 +94,9 @@ describe DittyApp, "< Sinatra::Application" do
     end
     it "should have title" do
       last_response.should match Regexp.new(Regexp.escape("<title>My little Ditty's!</title>"))
+    end
+    it "post title should be a link" do
+      last_response.should match Regexp.new("<a href=\'\/post\/([a-z0-9]+)'>post title - (.+)</a>")
     end
     it "should not have new post link" do
       last_response.should_not match Regexp.new(Regexp.escape("new post</a>"))
